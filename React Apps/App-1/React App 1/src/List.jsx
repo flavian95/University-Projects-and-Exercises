6b75f65f270e7460853badc1e85e536699e79f76
flavian95 <flavian.o7297@gmail.com>
2024-03-14 13:51:08 +0200

    message

---
+++ React Apps/App-1/React App 1/src/List.jsx
@@ -0,0 +1,31 @@
+import propTypes from 'prop-types';
+
+function List(props){
+
+   const category = props.category;
+   const itemList = props.items;
+
+   const listItems = itemList.map(item => <li key={item.id}>
+                                         {item.name}: &nbsp;
+                                         <b>{item.calories}</b></li>)
+
+   return (
+    <>
+     <h3 className="list-category">{category}</h3> 
+     <ol className="list-items">{listItems}</ol>
+   </>);
+}
+
+List.propTypes = {
+    category: propTypes.string,
+    items: propTypes.arrayOf(propTypes.shape({id: propTypes.number, 
+                            name: propTypes.string,
+                            calories: propTypes.number}))
+};
+
+List.defaultProps= {
+    category: "Category",
+    items: []
+}
+
+export default List
